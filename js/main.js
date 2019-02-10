063cf98b2d19b7942c98f7c56f6a7661cc18eb75
strom2019 <ovi0167@hotmail.com>
2019-02-10 12:59:48 +0600

    just file upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+ /*------------------------------------
+ *Author:MD ABU SAYED
+ *Template:Storrea Light
+ *Version:1.0
+ *-------------------------------------
+ */
+
+
+$(document).ready(function(){
+
+	// $(window).scroll(function(){
+	//     if ($(this).scrollTop()>150){
+	//       $('.menu').addClass('fixed-menu');
+	      
+	//     } 
+	//     else {
+	//       $('.menu').removeClass('fixed-menu');
+	//     }
+ //  	});
+
+  	// if ($(window).width()<768){
+   //    $('.col-logo').addClass('order-12');
+   //    $('.col-icon').addClass('order-1');
+      
+   //  } 
+   //  else {
+   //    $('.col-logo').removeClass('order-12');
+   //    $('.col-icon').removeClass('order-1');
+   //  }
+
+
+   // Start Home Slider Js
+
+ //    $('#main-slider').owlCarousel({
+ //    	// autoplay:true,
+ //    	autoplayHoverPause:true,
+ //    	dots:true,
+ //    	items:1,
+ //    	loop:true,
+	//     nav:false,
+	//     responsive:{
+	//         0:{
+	//             items:1
+	//         },
+	//         600:{
+	//             items:1
+	//         },
+	//         768:{
+	//             items:1
+	//         }
+	//     },
+	//     slideBy:1,
+	//     smartSpeed:1800
+	// })
+
+    // End Home Slider Js
+
+
+});
+
+
